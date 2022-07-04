aea1ff516a588d690a32785a8778ed0489748233
Manuel Gutiérrez <manu95gutierrez@gmail.com>
2022-07-04 19:37:02 -0300

    1

---
+++ RestoG6/src/restog6/resto.java
@@ -0,0 +1,16 @@
+
+package restog6;
+
+/**
+ * @author Grupo 6
+ * Fernandez Valentina
+ * Romero Jorge
+ * Manuel Gutierrez
+ */
+public class resto {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
