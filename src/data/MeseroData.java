8acb996da448c1e85d8b78445e710cdb164d0835
JorgeRomero-ULP <zhypres@gmail.com>
2022-07-12 15:42:06 -0300

    mesero y producto data
    se suben clases data

--- src/data/MeseroData.java
+++ src/data/MeseroData.java
@@ -1,5 +1,15 @@
 package data;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+import modelos.Mesero;
+
 /**
  * @author Grupo 6
  * Fernandez Valentina
@@ -10,12 +20,126 @@ public class MeseroData {
     //                                              ATRIBUTOS
     //---------------------------------------------------------------------------------------------------------------
     
+    private Connection conexion = null;
+    
     //                                  CONSTRUCTORES, GETTERS Y SETTERS
     //---------------------------------------------------------------------------------------------------------------
+    
+    public MeseroData(Conexion conexion){
+        this.conexion = conexion.getConexion();
+    }
     
     //                                          METODOS PUBLICOS
     //---------------------------------------------------------------------------------------------------------------
     
+    /**
+     * Agrega un mesero en la tabla mesero de la Base de Datos
+     * @param unMesero es el mesero que va a ser agregado
+     * @return true si se agrego correctamente, si no, false
+     */
+    public boolean agregarMesero(Mesero unMesero){
+        boolean agregado = false;        
+        String sql = "INSERT INTO mesero (nombreMesero, apellido, dni, telefono, activo)" 
+                   + "VALUES (?, ?, ?, ?, ?);";        
+        try{
+            PreparedStatement ps = conexion.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            
+            ps.setString(1, unMesero.getNombreMesero());
+            ps.setString(2, unMesero.getApellido());
+            ps.setString(3, unMesero.getDni());
+            ps.setString(4, unMesero.getTelefono());
+            ps.setBoolean(5, unMesero.isActivo());            
+            ps.executeUpdate();            
+            ResultSet result = ps.getGeneratedKeys();            
+            if(result.next()){
+                unMesero.setIdMesero(result.getInt("idMesero"));
+                agregado = true;
+            }            
+            ps.close();
+        }catch(SQLException sqle){
+            JOptionPane.showMessageDialog(null, "Error al agregar un mesero");
+        }        
+        return agregado;
+    }
+    
+     /**
+     * Borrado lógico de un mesero cuya id es pasada por parámetro
+     * @param id es el id del mesero a borrar
+     * @return true si se borro con éxito, si no, false
+     */
+    public boolean borrarMesero(int id){
+        boolean borrar = false;        
+        String sql = "UPDATE mesero "
+                   + "SET activo = 0 "
+                   + "WHERE idMesero = ?;";        
+        try{
+            PreparedStatement ps = conexion.prepareStatement(sql);            
+            ps.setInt(1, id);            
+            if(ps.executeUpdate() != 0){
+                borrar = true;
+            }            
+            ps.close();
+        }catch(SQLException sqle){
+            JOptionPane.showMessageDialog(null, "Error al borrar el mesero");
+        }        
+        return borrar;
+    }
+    
+    /**
+     * Modifica los datos de un mesero en la Base de Datos
+     * @param unMesero es el producto que se desea modificar
+     * @return true si se modifico con éxito, si no, false
+     */
+    public boolean modificarMesero(Mesero unMesero){
+        boolean modificado = false;        
+        String sql = "UPDATE mesero "
+                   + "SET nombre = ?, apellido = ?, dni = ?, telefono = ?, estado = ? "
+                   + "WHERE idMesero = ?";        
+        try{
+            PreparedStatement ps = conexion.prepareStatement(sql);            
+            ps.setString(1, unMesero.getNombreMesero());
+            ps.setString(2, unMesero.getApellido());
+            ps.setString(3, unMesero.getDni());
+            ps.setString(4, unMesero.getTelefono());
+            ps.setBoolean(5, unMesero.isActivo());
+            ps.setInt(6, unMesero.getIdMesero());            
+            if(ps.executeUpdate() != 0){
+                modificado = true;
+            }            
+            ps.close();
+        }catch(SQLException sqle){
+            JOptionPane.showMessageDialog(null, "Error al modificar los datos del mesero");
+        }        
+        return modificado;
+    }
+    
+    /**
+     * Devuelve todos los meseros
+     * @return un ArrayList con todos los meseros, o un ArrayList vacío
+     */
+    public List<Mesero> todosLosMeseros(){
+        ArrayList<Mesero> meseros = new ArrayList<>();        
+        String sql = "SELECT * FROM mesero WHERE activo = 1";        
+        try{
+            PreparedStatement ps = conexion.prepareStatement(sql);            
+            ResultSet result = ps.executeQuery();            
+            Mesero unMesero;
+            while(result.next()){
+                unMesero = new Mesero();
+                unMesero.setIdMesero(result.getInt("idMesero"));
+                unMesero.setNombreMesero(result.getString("nombre"));
+                unMesero.setApellido(result.getString("apellido"));
+                unMesero.setDni(result.getString("dni"));
+                unMesero.setTelefono(result.getString("telefono"));
+                unMesero.setActivo(result.getBoolean("activo"));                
+                meseros.add(unMesero);
+            }            
+            ps.close();
+        }catch(SQLException sqle){
+            JOptionPane.showMessageDialog(null, "Error al obtener la lista de meseros");
+        }        
+        return meseros;
+    }
     //                                          METODOS PRIVADOS
     //---------------------------------------------------------------------------------------------------------------
 }
--- src/data/ProductoData.java
+++ src/data/ProductoData.java
@@ -1,6 +1,16 @@
 
 package data;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+import modelos.Producto;
+
 /**
  * @author Grupo 6
  * Fernandez Valentina
@@ -11,11 +21,123 @@ public class ProductoData {
     //                                              ATRIBUTOS
     //---------------------------------------------------------------------------------------------------------------
     
+    private Connection conexion = null;
+    
     //                                  CONSTRUCTORES, GETTERS Y SETTERS
     //---------------------------------------------------------------------------------------------------------------
     
+    public ProductoData(Conexion conexion){
+        this.conexion = conexion.getConexion();
+    }
+    
     //                                          METODOS PUBLICOS
     //---------------------------------------------------------------------------------------------------------------
+    
+    /**
+     * Agrega un producto en la tabla producto de la Base de Datos
+     * @param unProducto es el producto que va a ser agregado
+     * @return true si se agrego correctamente, si no, false
+     */
+    public boolean agregarProducto(Producto unProducto){
+        boolean agregado = false;        
+        String sql = "INSERT INTO producto(nombreProducto, precio, stock, activo)" 
+                   + " VALUES (?, ?, ?, ?)";        
+        try{
+            PreparedStatement ps = conexion.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            
+            ps.setString(1, unProducto.getNombreProducto());
+            ps.setDouble(2, unProducto.getPrecio());
+            ps.setInt(3, unProducto.getStock());
+            ps.setBoolean(4, unProducto.isActivo());            
+            ps.executeUpdate();            
+            ResultSet result = ps.getGeneratedKeys();            
+            if(result.next()){
+                unProducto.setIdProducto(result.getInt("idProducto"));
+                agregado = true;
+            }            
+            ps.close();
+        }catch(SQLException sqle){
+            JOptionPane.showMessageDialog(null, "Error al agregar un producto");
+        }        
+        return agregado;
+    }
+    
+     /**
+     * Borrado lógico de un producto cuya id es pasada por parámetro
+     * @param id es el id del producto a borrar
+     * @return true si se borro con éxito, si no, false
+     */
+    public boolean borrarProducto(int id){
+        boolean borrar = false;        
+        String sql = "UPDATE producto "
+                   + "SET activo = 0 "
+                   + "WHERE idProducto = ?;";        
+        try{
+            PreparedStatement ps = conexion.prepareStatement(sql);            
+            ps.setInt(1, id);            
+            if(ps.executeUpdate() != 0){
+                borrar = true;
+            }            
+            ps.close();
+        }catch(SQLException sqle){
+            JOptionPane.showMessageDialog(null, "Error al borrar el producto");
+        }        
+        return borrar;
+    }
+    
+    /**
+     * Modifica los datos de un producto en la Base de Datos
+     * @param unProducto es el producto que se desea modificar
+     * @return true si se modifico con éxito, si no, false
+     */
+    public boolean modificarProducto(Producto unProducto){
+        boolean modificado = false;        
+        String sql = "UPDATE producto "
+                   + "SET nombreProducto = ?, precio = ?, stock = ?, activo = ? "
+                   + "WHERE idProducto = ?";        
+        try{
+            PreparedStatement ps = conexion.prepareStatement(sql);            
+            ps.setString(1, unProducto.getNombreProducto());
+            ps.setDouble(2, unProducto.getPrecio());
+            ps.setInt(3, unProducto.getStock());
+            ps.setBoolean(4, unProducto.isActivo());
+            ps.setInt(5, unProducto.getIdProducto());            
+            if(ps.executeUpdate() != 0){
+                modificado = true;
+            }            
+            ps.close();
+        }catch(SQLException sqle){
+            JOptionPane.showMessageDialog(null, "Error al modificar el producto");
+        }        
+        return modificado;
+    }
+    
+    /**
+     * Devuelve todos los productos
+     * @return un ArrayList con todos los productos, o un ArrayList vacío
+     */
+    public List<Producto> todosLosProductos(){
+        ArrayList<Producto> productos = new ArrayList<>();        
+        String sql = "SELECT * FROM producto WHERE activo = 1";        
+        try{
+            PreparedStatement ps = conexion.prepareStatement(sql);            
+            ResultSet result = ps.executeQuery();            
+            Producto unProducto;
+            while(result.next()){
+                unProducto = new Producto();
+                unProducto.setIdProducto(result.getInt("idProducto"));
+                unProducto.setNombreProducto(result.getString("nombreProducto"));
+                unProducto.setPrecio(result.getDouble("precio"));
+                unProducto.setStock(result.getInt("stock"));
+                unProducto.setActivo(result.getBoolean("activo"));                
+                productos.add(unProducto);
+            }            
+            ps.close();
+        }catch(SQLException sqle){
+            JOptionPane.showMessageDialog(null, "Error al obtener los productos");
+        }        
+        return productos;
+    }
     
     //                                          METODOS PRIVADOS
     //---------------------------------------------------------------------------------------------------------------
